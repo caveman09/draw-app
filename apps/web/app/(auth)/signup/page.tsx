fb6690c6facdb0aad3d6ac3080d811c49ede072c
caveman <anshumans09@hotmail.com>
2025-01-18 02:21:00 +0530

    SignUp component

--- apps/web/app/(auth)/signup/page.tsx
+++ apps/web/app/(auth)/signup/page.tsx
@@ -1,19 +1,85 @@
+"use client"
 import { Card, CardHeader, CardDescription, CardTitle, CardContent } from "@/components/ui/card";
+import { CreateUserSchema } from "@repo/common/types";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input";
 
 export default function Page() {
+    const form = useForm<z.infer<typeof CreateUserSchema>>({
+        resolver: zodResolver(CreateUserSchema),
+        defaultValues: {
+            username: "",
+            password: "",
+            name: ""
+        }
+    });
+
+    function onSubmit(values: z.infer<typeof CreateUserSchema>) {
+        // do something here
+    }
+
     return (
         <Card>
             <CardHeader>
-                <CardTitle>
-                    Signup
+                <CardTitle className="font-semibold">
+                    SignUp
                 </CardTitle>
                 <CardDescription>
-                    Create an account for free.
+                    Create a free account.
                 </CardDescription>
             </CardHeader>
 
             <CardContent>
-
+                <Form {...form}>
+                    <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-2">
+                        <FormField control={form.control} name="username" render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="font-medium">
+                                    Email
+                                </FormLabel>
+                                <FormControl>
+                                    <Input placeholder="johndoe@cavemail.com" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )} />
+                        <FormField control={form.control} name="password" render={({ field }) => (
+                            <FormItem className="">
+                                <FormLabel className="font-medium">
+                                    Password
+                                </FormLabel>
+                                <FormControl className="">
+                                    <Input placeholder="password123" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )} />
+                        <FormField control={form.control} name="name" render={({ field }) => (
+                            <FormItem className="">
+                                <FormLabel className="font-medium">
+                                    Display Name
+                                </FormLabel>
+                                <FormControl className="">
+                                    <Input placeholder="JohnDoe" {...field} />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )} />
+                        <Button type="submit">Create Account</Button>
+                    </form>
+                </Form>
             </CardContent>
         </Card>);
 }
