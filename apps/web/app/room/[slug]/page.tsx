38f5889e34ba17980f3f50560396a735070b68a4
caveman <anshumans09@hotmail.com>
2025-01-22 18:38:28 +0530

    customized shadcn's sheet component, have to work more on this

--- apps/web/app/room/[slug]/page.tsx
+++ apps/web/app/room/[slug]/page.tsx
@@ -1,11 +1,40 @@
 import LeaveButton from "./leaveButton";
+import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+} from "@/components/ui/sheetWindow"
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
+import { Card } from "@/components/ui/card";
 
 export default async function Page({ params }: { params: { slug: string } }) {
     const slug = (await params).slug;
     return (
-        <div className="text-white">
-            ROOM {`${slug}`}
-            <LeaveButton slug={`${slug}`} />
-        </div>
+        <div className="text-white p-2">
+            <Sheet>
+                <SheetTrigger className="z-50">
+                    <Avatar>
+                        <AvatarImage src="https://github.com/shadcn.png" />
+                        <AvatarFallback>A</AvatarFallback>
+                    </Avatar>
+                </SheetTrigger>
+                <SheetContent side={'topleft'} className="rounded-lg">
+                    <SheetHeader>
+                        <SheetTitle>Are you absolutely sure?</SheetTitle>
+                        <SheetDescription>
+                            This action cannot be undone. This will permanently delete your account
+                            and remove your data from our servers.
+                        </SheetDescription>
+                    </SheetHeader>
+                </SheetContent>
+            </Sheet >
+            <div className="flex justify-center">
+                ROOM {`${slug}`}
+                <LeaveButton slug={`${slug}`} />
+            </div>
+        </div >
     );
 }
