6793b58cb719224c3bb9bc3fe4800856f6ca5a55
caveman <anshumans09@hotmail.com>
2025-01-22 22:35:54 +0530

    fixed animations for the sheetWindow

--- apps/web/app/components/ui/sheetWindow.tsx
+++ apps/web/app/components/ui/sheetWindow.tsx
@@ -19,36 +19,21 @@ const SheetOverlay = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Overlay>,
   React.ComponentPropsWithoutRef<typeof SheetPrimitive.Overlay>
 >(({ className, ...props }, ref) => {
-  const innerRef = React.useRef<HTMLDivElement>(null);
-  const [state, setState] = React.useState('closed');
-
-  React.useEffect(() => {
-    if (innerRef.current) {
-      requestAnimationFrame(() => {
-        requestAnimationFrame(() => {
-          setState('open');
-          console.log('open');
-        });
-      });
-    }
-  }, []);
 
   return (
     <SheetPrimitive.Overlay
       className={cn(
-        "fixed inset-0 z-40 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
+        "fixed inset-0 z-40 data-[state=open]:animate-in data-[state=closed]:animate-out",
         className
       )}
       {...props}
-      data-state={state}
-      ref={innerRef}
     />
   );
 })
 SheetOverlay.displayName = SheetPrimitive.Overlay.displayName
 
 const sheetVariants = cva(
-  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition-all ease-in-out data-[state=closed]:duration-300 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out",
+  "fixed z-50 gap-4 bg-background p-6 shadow-lg transition-all ease-in-out data-[state=closed]:duration-500 data-[state=open]:duration-500 data-[state=open]:animate-in data-[state=closed]:animate-out",
   {
     variants: {
       side: {
@@ -57,8 +42,9 @@ const sheetVariants = cva(
           "inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom",
         left: "inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm",
         right:
-          "inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
-        topleft: "inset-x-2 inset-y-2 border-r data-[state=closed]:w-8 data-[state=closed]:h-8 data-[state=open]:w-[400px] data-[state=open]:h-[400px] sm:max-w-sm"
+          "inset-y-0 right-0 h-8 w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm",
+        topleft: "inset-x-2 inset-y-2 border-r data-[state=closed]:w-8 data-[state=closed]:h-8 data-[state=open]:w-8 data-[state=open]:h-8 sm:max-w-sm",
+        activated: "inset-x-2 inset-y-2 border-r data-[state=closed]:w-8 data-[state=closed]:h-8 data-[state=open]:w-[400px] data-[state=open]:h-[400px] sm:max-w-sm"
       },
     },
     defaultVariants: {
@@ -74,22 +60,45 @@ interface SheetContentProps
 const SheetContent = React.forwardRef<
   React.ElementRef<typeof SheetPrimitive.Content>,
   SheetContentProps
->(({ side = "right", className, children, ...props }, ref) => (
-  <SheetPortal>
-    <SheetOverlay />
-    <SheetPrimitive.Content
-      ref={ref}
-      className={cn(sheetVariants({ side }), className)}
-      {...props}
-    >
-      <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
-        <X className="h-4 w-4" />
-        <span className="sr-only">Close</span>
-      </SheetPrimitive.Close>
-      {children}
-    </SheetPrimitive.Content>
-  </SheetPortal>
-))
+>(({ side = "right", className, children, ...props }, ref) => {
+  const [activated, setActivated] = React.useState(false);
+  const innerRef = React.useRef<HTMLDivElement>(null);
+
+  const setRef = (element: HTMLDivElement) => {
+    if (element) {
+      console.log('Ref is valid:', element);
+      setActivated(true);
+    } else {
+      console.log('Ref is invalid:', element);
+      setActivated(false);
+    }
+
+    if (ref) {
+      if (typeof ref === 'function') {
+        ref(element);
+      } else {
+        ref.current = element;
+      }
+    }
+  }
+
+  return (
+    <SheetPortal>
+      <SheetOverlay />
+      <SheetPrimitive.Content
+        ref={setRef}
+        className={cn(sheetVariants(activated ? { side: "activated" } : { side }), className)}
+        {...props}
+      >
+        <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
+          <X className="h-4 w-4" />
+          <span className="sr-only">Close</span>
+        </SheetPrimitive.Close>
+        {children}
+      </SheetPrimitive.Content>
+    </SheetPortal>
+  );
+})
 SheetContent.displayName = SheetPrimitive.Content.displayName
 
 const SheetHeader = ({
